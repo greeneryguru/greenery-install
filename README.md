9f4de21d9cd119f7c8aa57e9fd741e9f9e1b7afe
Jeffrey Leary <jeffreyleary@Jeffreys-MacBook-Air.local>
2019-06-11 20:29:56 -0400

    doc updates and script improvements

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
-# potnanny-install
-Installation scripts for the PotNanny Raspberry Pi system.
+# POTNANNY-INSTALL
+Installation scripts for the Potnanny greenhouse automation system for Raspberry Pi.
+
+See the main [PotNanny](https://github.com/jeffleary00/potnanny) project for more details and documentation.
+
+
+## Download Raspbian Lite.
+At time of this document, Raspbian 'Stretch' is the latest version and is recommended for this install.
+
+
+## Burn Raspbian Image to MicroSD card.
+There are many ways to do this but [Balena Etcher](https://www.balena.io/etcher/) is a free app and very popular. It is one of our favorites.
+
+
+## Create SSH file.
+Once the Raspbian image has been burned to the MicroSD card, create a plain file called "ssh" in the top-level folder of the MicroSD card. This file can, and should, be empty. It simply needs to exist. *It must not have any file-type extension added (.txt, .doc, etc)*.
+
+Mac or Linux users may want to do this from the command line:
+```
+touch ssh
+```
+
+
+## Edit the config file.
+Use a text editor (see notes below for Windows users), and add the following line to the end of the file named "config.txt"
+```
+dtoverlay=dwc2
+```
+
+
+## Edit the cmdline file.
+Use a text editor and add the following entry to the very end of the single line in file called "cmdline.txt"
+```
+modules-load=dwc2,g_ether
+```
+
+
+## Eject and insert.
+Safely eject the MicroSD card from your computer and insert into the Raspberry Pi.
+
+
+## Connect USB.
+Plug USB cable from your computer to the mini/micro USB port marked "USB" on the Pi.
+It may take a minute before the Raspberry Pi fully boots, and the SSH service is available.
+
+
+## Connect via terminal.
+Using a terminal emulator app, connect to the Pi with the following ssh command:
+```
+ssh pi@raspberrypi.local
+```
+Enter Yes to confirm you want to connect to the host.
+Enter password "raspberry"
+You're in.
+
+
+## Configure WiFi Networking.
+```
+sudo raspi-config
+```
+Select option 2 "Network Options"
+
+You will need to enter:
+ 1. The country.
+ 2. The WiFi SSID name.
+ 3. The WiFi passphrase
+
+Select FINISHED when completed.
+*NOTE: Use the TAB and ARROW keys to move the menu cursor to other parts of the screen.*
+
+
+## Get the IP address of the Raspberry Pi.
+```
+ifconfig -a | grep "inet " | grep -v "127.0.0"
+```
+The output of this command will look something like:
+```
+inet 192.168.1.3  netmask 255.255.255.0  broadcast 192.168.1.255
+```
+
+You need to know the "inet" address (192.168.1.3, in this case), for connecting to the web interface later.
+
+
+## Install GIT.
+```
+sudo apt-get -y install git
+```
+
+
+## Clone the Potnanny install project, and run the installer script.
+```
+git clone https://github.com/jeffleary00/potnanny-install
+cd potnanny-install
+bash ./install.bash
+```
+
+
+## Begin using your new Potnanny system!
+The Potnanny system can now be accessed via web browser at the URL;
+```
+https://IPADDRESS
+```
+Where IPADDRESS is the IPv4 inet address of the Raspberry Pi, as captured in steps above.
+
+ - Login username/password = 'potnanny/potnanny'
+
+You should change this account password immediately! Go to the account settings page.
+
+
+### Notes for Windows Users.
+Do not EVER use Microsoft's "Notepad" app to edit the config files. This program will render the files unreadable to the Raspbian OS. You should use an editor that is capable of handling Linux/Unix formatted files.
+
+Text editor options for Windows users:
+ - [gEdit](https://wiki.gnome.org/Apps/Gedit)
+ - [Notepad ++](https://notepad-plus-plus.org)
+
+Terminal emulator options for Windows users:
+ - [MobaXterm](https://mobaxterm.mobatek.net)
+ - [Putty](https://www.chiark.greenend.org.uk/~sgtatham/putty/)
--- install.bash
+++ install.bash
@@ -2,12 +2,16 @@
 
 # -----------------------------------------------------------------------------
 #
-# Run this as the 'pi' user on Raspberry Pi
+# Run this script as the 'pi' user on Raspberry Pi
+#
+# Synopsis:
+# pi@raspberrypi:$  bash ./install.bash
 #
 # -----------------------------------------------------------------------------
 
 
-echo "INSTALL LIBRARIES AND APPLICATIONS"
+echo "---------------------------------------------------------"
+echo "INSTALL SYSTEM LIBRARIES AND APPLICATIONS"
 echo "---------------------------------------------------------"
 sudo apt-get update
 sudo apt-get -y install build-essential
@@ -21,9 +25,11 @@ sudo apt-get -y install nginx
 sudo apt-get -y install uwsgi
 sudo apt-get -y install uwsgi-plugin-python3
 sudo apt-get -y install sqlite
+sudo apt-get -y install nodejs npm
 
 
-echo "INSTALL PYTHON MODULES"
+echo "---------------------------------------------------------"
+echo "INSTALL BASE PYTHON MODULES"
 echo "---------------------------------------------------------"
 sudo pip3 install sqlalchemy
 sudo pip3 install marshmallow
@@ -32,80 +38,179 @@ sudo pip3 install flask-restful
 sudo pip3 install flask-jwt-extended
 sudo pip3 install bluepy
 sudo pip3 install btlewrap
-sudo pip3 install miflora
-sudo pip3 install mitemp_bt
-sudo pip3 install vesync-outlet
 
 
-echo "CONFIGURE UWSGI"
 echo "---------------------------------------------------------"
-sudo cp ./uwsgi/potnanny.ini /etc/uwsgi/apps-available
-sudo ln -s /etc/uwsgi/apps-available/potnanny.ini /etc/uwsgi/apps-enabled/potnanny.ini
-
-
-echo "CONFIGURE NGINX"
-echo "---------------------------------------------------------"
-sudo cp ./nginx/default /etc/uwsgi/sites-available
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-
-
-echo "CONFIGURE DIRECTORIES"
-echo "---------------------------------------------------------"
-sudo mkdir /var/log/potnanny
-sudo touch /var/log/potnanny/poll.log
-sudo chown $USER /var/log/potnanny/poll.log
-sudo chgrp www-data /var/log/potnanny/poll.log
-sudo chmod 664 /var/log/potnanny/poll.log
-
-
-echo "CONFIGURE LOGROTATE"
-echo "---------------------------------------------------------"
-sudo sh -c 'cat ./logrotate.conf >>/etc/logrotate.conf'
-
-
 echo "INSTALL WIRINGPI FROM SOURCE"
 echo "---------------------------------------------------------"
 cd /tmp
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 git pull origin
-./build
+sudo ./build
 
 
-echo "INSTALL POTNANNY FROM SOURCE"
 echo "---------------------------------------------------------"
-cd /var/www
-sudo git clone https://github.com/jeffleary00/potnanny.git
-cd potnanny
-sudo pip3 install -e .
+echo "SETUP POTNANNY DIRECTORIES"
+echo "---------------------------------------------------------"
+DIRLIST="
+  /var/log/potnanny
+  /var/local/potnanny
+  /opt/potnanny
+  /opt/potnanny/plugins
+  /opt/potnanny/plugins/action
+  /opt/potnanny/plugins/ble"
+
+for DIR in $DIRLIST
+do
+  if [ ! -d $DIR ]; then
+    sudo mkdir $DIR
+  fi
+  sudo chown root $DIR
+  sudo chgrp www-data $DIR
+  sudo chmod 775 $DIR
+done
 
 
-echo "SET PERMISSIONS"
+echo "---------------------------------------------------------"
+echo "SETUP LOG FILES"
+echo "---------------------------------------------------------"
+LOGLIST="
+  /var/log/potnanny/potnanny.log"
+
+for FNAME in $LOGLIST
+do
+  if [ ! -e $FNAME ]; then
+    sudo touch $FNAME
+  fi
+  sudo chown $USER $FNAME
+  sudo chgrp www-data $FNAME
+  sudo chmod 664 $FNAME
+done
+
+
+echo "---------------------------------------------------------"
+echo "INSTALL POTNANNY CORE"
+echo "---------------------------------------------------------"
+# no-binary must be used, to get around weird bug in setuptools, where all
+# data_files are installed relative to package, in system python dir.
+# This flag ensures non-package files get installed to correct filesystem paths.
+sudo pip3 install --no-binary :all: potnanny-core==0.3.0
+
+
+echo "---------------------------------------------------------"
+echo "INSTALL POTNANNY API"
+echo "---------------------------------------------------------"
+sudo pip3 install potnanny-api==0.3.0
+
+
+echo "---------------------------------------------------------"
+echo "CONFIGURE LOGROTATE"
+echo "---------------------------------------------------------"
+if [ ! -e "/etc/logrotate.conf" ]; then
+  sudo touch /etc/logrotate.conf
+fi
+sudo sh -c 'cat ./logrotate.conf >>/etc/logrotate.conf'
+
+
+echo "---------------------------------------------------------"
+echo "CONFIGURE UWSGI"
+echo "---------------------------------------------------------"
+sudo cp ./uwsgi/potnanny.ini /etc/uwsgi/apps-available
+sudo ln -s /etc/uwsgi/apps-available/potnanny.ini /etc/uwsgi/apps-enabled/potnanny.ini
+
+
+echo "---------------------------------------------------------"
+echo "CONFIGURE NGINX"
+echo "---------------------------------------------------------"
+sudo cp ./nginx/default /etc/uwsgi/sites-available
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+
+
+echo "---------------------------------------------------------"
+echo "SET WWW PERMISSIONS"
 echo "---------------------------------------------------------"
 sudo usermod -a -G www-data $USER
-sudo touch /var/www/potnanny/sqlite.db
 cd /var/www
 sudo chown -R www-data potnanny
 sudo chgrp -R www-data potnanny
-sudo chmod 660 /var/www/potnanny/sqlite.db
 
 
-echo "RESTART WEB SERVICES"
 echo "---------------------------------------------------------"
+echo "SET ADDITIONAL PERMISSIONS"
+echo "---------------------------------------------------------"
+DIRLIST="
+  /opt/potnanny/plugins"
+
+for DIR in $DIRLIST
+do
+  sudo chgrp -R www-data $DIR
+  sudo chmod -R 774 $DIR
+done
+
+FLIST="
+  /usr/local/bin/rf_send
+  /usr/local/bin/rf_send"
+
+for FNAME in $FLIST
+do
+  sudo chgrp www-data $FNAME
+  sudo chmod 770 $FNAME
+done
+
+
+echo "---------------------------------------------------------"
+echo "INSTALL POTNANNY CRON JOB"
+echo "---------------------------------------------------------"
+if [ ! -e /var/spool/cron/crontabs/$USER ]; then
+  sudo touch /var/spool/cron/crontabs/$USER
+fi
+sudo cat /var/spool/cron/crontabs/$USER | grep "potnanny poll"
+if [ $? -ne 0 ]; then
+  sudo sh -c "echo '* * * * * potnanny poll' >>/var/spool/cron/crontabs/$USER"
+fi
+sudo chmod 0600 /var/spool/cron/crontabs/$USER
+sudo chown $USER /var/spool/cron/crontabs/$USER
+
+
+echo "---------------------------------------------------------"
+echo "GENERATE SELF-SIGNED CERT FOR NGINX"
+echo "HELP: You can enter just a '.' for each question"
+echo "---------------------------------------------------------"
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+
+
+echo "---------------------------------------------------------"
+echo "GENERATE SSL DH GROUP FOR NGINX (background process)"
+echo "---------------------------------------------------------"
+sudo openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 2048 >/dev/null 2>&1 &
+DHPID=$!
+
+
+echo "---------------------------------------------------------"
+echo "SETUP INITIAL POTNANNY DATABASE"
+echo "---------------------------------------------------------"
+potnanny db init
+
+
+echo "---------------------------------------------------------"
+echo "RESTART WWW SERVICES"
+echo "---------------------------------------------------------"
+while :
+do
+  RVAL=`ps -p $DHPID`
+  if [ $? != 0 ]; then
+    break
+  else
+    echo "waiting for SSL DH background process to finish. takes a long time..."
+    sleep 60
+  fi
+done
 sudo service nginx restart
 sudo service uwsgi restart
 
 
-echo "INSTALL POTNANNY CRON JOB"
-echo "---------------------------------------------------------"
-sudo touch /var/spool/cron/crontabs/$USER
-sudo cat /var/spool/cron/crontabs/$USER | grep "poll.py"
-if [ $? -ne 0 ]; then
-    sudo sh -c "echo '* * * * * /var/www/potnanny/bin/poll.py' >>/var/spool/cron/crontabs/$USER"
-fi
-sudo chmod 0600 /var/spool/cron/crontabs/$USER
-
-
+echo ""
 echo "========================================================="
 echo "COMPLETE"
 echo "========================================================="
